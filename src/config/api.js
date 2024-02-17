7fe3a13b645b6ca8b1d0f27d57ca4ad2fe863e71
Shabab <shabab@gmail.com>
2024-02-17 17:09:06 +0600

    a

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL="http://localhost:8080";
+export const API_BASE_URL="socialmedia-server-production.up.railway.app";
+
 
 const jwtToken=localStorage.getItem("jwt")
 
